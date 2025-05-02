161687e5d4e4b7730584b0e49159d1442fb4b34e
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-02 02:20:02 -0500

    LinkedList class initializer with tests.

--- linked_list.py
+++ linked_list.py
@@ -2,3 +2,8 @@ class Node:
     def __init__(self, value) -> None:
         self.data = value
         self.nxt = None
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
