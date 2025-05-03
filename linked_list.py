b2e57cb2cca9f5994c3de4fab4d30dcb0e3cd60d
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-02 22:49:23 -0500

    Push method for linked list with tests.

--- linked_list.py
+++ linked_list.py
@@ -1,9 +1,19 @@
 class Node:
-    def __init__(self, value) -> None:
+    def __init__(self, value, nxt=None) -> None:
         self.data = value
-        self.nxt = None
+        self.nxt = nxt
 
 
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+
+        new_node.nxt = self.head
+        self.head = new_node
+        return
--- test_linked_list.py
+++ test_linked_list.py
@@ -3,6 +3,24 @@ import pytest
 from linked_list import LinkedList, Node
 
 
+@pytest.fixture
+def one_node():
+    lst = LinkedList()
+    lst.push(1)
+    return lst
+
+
+@pytest.fixture
+def lst_factory(request):
+    marker = request.node.get_closest_marker("lst_factory_data")
+
+    # Do something with the data
+    lst = LinkedList()
+    for value in marker.args[0]:
+        lst.push(value)
+    return lst
+
+
 class TestNode:
     def test_node_initiates_with_data_attribute(self):
         """A node object initializes with the 'data' attribute."""
@@ -40,3 +58,28 @@ class TestLinkedListClassHead:
         """A LinkedList initializes with the .head attribute."""
         lst = LinkedList()
         assert not lst.head
+
+
+class TestPushMethod:
+    def test_LinkedList_with_1_node_is_head(self, one_node):
+        """Push 1 value into Linked List, that value should be the head's data"""
+        assert one_node.head.data == 1
+
+    def test_LinkedList_with_1_node_head_nxt_is_none(self, one_node):
+        """Push 1 value into Linked List, head.nxt should be None"""
+        assert not one_node.head.nxt
+
+    @pytest.mark.lst_factory_data([1, 2])
+    def test_LinkedList_with_2_nodes_correct_head(self, lst_factory):
+        """Push 2 values into Linked List, last value pushed should be the head's data"""
+        assert lst_factory.head.data == 2
+
+    @pytest.mark.lst_factory_data([1, 2])
+    def test_LinkedList_with_2_nodes_correct_head_nxt_data(self, lst_factory):
+        """Push 2 values into Linked List, first value pushed should be head.nxt.data"""
+        assert lst_factory.head.nxt.data == 1
+
+    @pytest.mark.lst_factory_data([1, 2])
+    def test_LinkedList_with_2_nodes_no_3rd_node(self, lst_factory):
+        """Push 2 values into Linked List, there should be no 3rd node"""
+        assert not lst_factory.head.nxt.nxt
