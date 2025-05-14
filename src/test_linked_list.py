350498a78afed0af6e7f437d83f4115934d30d51
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-14 18:04:13 -0500

    Initial commit of conftest.py
    Move fixtures from test_linked_list.py file into their own conftest.py file. Keeps it cleaner and prepare for additional datasrutures to come.

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -1,43 +1,6 @@
 import pytest
 
 from linked_list import LinkedList, Node
-import linked_list
-
-
-@pytest.fixture
-def one_node():
-    lst = LinkedList()
-    lst.push(1)
-    return lst
-
-
-@pytest.fixture
-def one_node_appended():
-    lst = LinkedList()
-    lst.append(1)
-    return lst
-
-
-@pytest.fixture
-def lst_factory(request):
-    marker = request.node.get_closest_marker("lst_factory_data")
-
-    # Do something with the data
-    lst = LinkedList()
-    for value in marker.args[0]:
-        lst.push(value)
-    return lst
-
-
-@pytest.fixture
-def lst_factory_append(request):
-    marker = request.node.get_closest_marker("lst_factory_data")
-
-    # Do something with the data
-    lst = LinkedList()
-    for value in marker.args[0]:
-        lst.push(value)
-    return lst
 
 
 class TestNode:
